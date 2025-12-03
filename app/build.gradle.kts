fe59c8ddb3f40fe7f94fd6190ecdfe509ded90bb
AlyaAzzahra1 <alyaazzahra1802@gmail.com>
2025-12-03 22:44:30 +0700

    Commit No. 1 – Init template

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,22 +37,28 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.recyclerview)
     implementation(libs.glide)
+
     implementation(libs.retrofit)
     implementation(libs.converter.moshi)
+
+    implementation(libs.moshi)
+    kapt(libs.moshi.kotlin.codegen)
+
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
-    kapt(libs.moshi.kotlin.codegen)
+
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
